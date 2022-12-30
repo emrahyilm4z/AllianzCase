38a2c397ed2118b36d7063c8e73e0fd366fd658a
emrahyilm4z <emrahyilm4z@gmail.com>
2022-12-30 12:05:47 +0300

    Initial commit

---
+++ src/main/java/com/example/emrah/EmrahApplication.java
@@ -0,0 +1,13 @@
+package com.example.emrah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emrah/EmrahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emrah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmrahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
