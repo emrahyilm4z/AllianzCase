3936ad4e1692f0ac54cfcf4ba8fa8d616add7ed0
emrahyilm4z <emrahyilm4z@gmail.com>
2023-01-05 01:55:07 +0300

    Complate test

--- src/main/java/com/example/emrah/dto/request/CampaignRequestDto.java
+++ src/main/java/com/example/emrah/dto/request/CampaignRequestDto.java
@@ -1,8 +1,10 @@
 package com.example.emrah.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CampaignRequestDto {
     private Long id;
 }
--- src/test/java/com/example/emrah/service/CampaignServiceTest.java
+++ src/test/java/com/example/emrah/service/CampaignServiceTest.java
@@ -1,17 +1,23 @@
 package com.example.emrah.service;
 
 import com.example.emrah.dto.request.AddCampaignRequestDto;
+import com.example.emrah.dto.request.CampaignRequestDto;
 import com.example.emrah.dto.response.CampaignResponseDto;
+import com.example.emrah.dto.response.ChangesResponseDto;
+import com.example.emrah.dto.response.StatisticsResponseDto;
 import com.example.emrah.entities.Campaign;
 import com.example.emrah.entities.Category;
 import com.example.emrah.repository.CampaignRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -32,20 +38,85 @@ public class CampaignServiceTest {
 
     @Test
     public void testAdd_WithValidRequest_ShouldReturnCampaignResponseDto() {
-        AddCampaignRequestDto addCampaignRequestDto = AddCampaignRequestDto.builder()
-                .title("heheheh")
-                .category(Category.OZEL_SAGLIK_SIGORTASI)
-                .description("ahehahehahe")
-                .build();
-        Campaign campaign = new Campaign(1L, "2", "23", false, false, Category.OZEL_SAGLIK_SIGORTASI);
-        CampaignResponseDto campaignResponseDto = new CampaignResponseDto("2", "23");
+        AddCampaignRequestDto addCampaignRequestDto = AddCampaignRequestDto.builder().title("heheheh").category(Category.OZEL_SAGLIK_SIGORTASI).description("ahehahehahe").build();
 
+        Campaign campaign = new Campaign(1L, "heheheh", "ahehahehahe", false, false, Category.OZEL_SAGLIK_SIGORTASI);
+        CampaignResponseDto campaignResponseDto = new CampaignResponseDto("heheheh", "ahehahehahe");
+
+
+        Mockito.when(modelMapper.map(addCampaignRequestDto, Campaign.class)).thenReturn(campaign);
         Mockito.when(campaignRepository.save(campaign)).thenReturn(campaign);
-
-
+        Mockito.when(modelMapper.map(campaign, CampaignResponseDto.class)).thenReturn(campaignResponseDto);
         CampaignResponseDto result = campaignService.add(addCampaignRequestDto);
         assertEquals(result, campaignResponseDto);
-        assertThat(campaign).isNotNull();
+
     }
 
+    @Test
+    public void testCategoryActivate() {
+        CampaignRequestDto campaignRequestDto = new CampaignRequestDto(1L);
+        Campaign campaign = new Campaign(1L, "heheheh", "ahehahehahe", false, false, Category.OZEL_SAGLIK_SIGORTASI);
+        CampaignResponseDto campaignResponseDto = new CampaignResponseDto("heheheh", "ahehahehahe");
+        Long id = 1L;
+
+        Mockito.when(campaignRepository.findById(id)).thenReturn(Optional.of(campaign));
+        Mockito.when(campaignRepository.save(campaign)).thenReturn(campaign);
+        Mockito.when(modelMapper.map(campaign, CampaignResponseDto.class)).thenReturn(campaignResponseDto);
+
+        CampaignResponseDto result = campaignService.categoryActivate(campaignRequestDto);
+
+        assertEquals(result, campaignResponseDto);
+
+    }
+
+
+    @Test
+    public void testGetAll() {
+        StatisticsResponseDto statisticsResponseDto = new StatisticsResponseDto(2, 1);
+
+        Campaign campaign = new Campaign();
+        List<Campaign> act = new ArrayList<>(2);
+        act.add(campaign);
+        act.add(campaign);
+        List<Campaign> dec = new ArrayList<>(1);
+        dec.add(campaign);
+
+        Mockito.when(campaignRepository.findByActiveIsTrue()).thenReturn(act);
+        Mockito.when(campaignRepository.findByActiveIsFalse()).thenReturn(dec);
+
+        StatisticsResponseDto result = campaignService.getAll();
+        assertEquals(result, statisticsResponseDto);
+    }
+
+    @Test
+    public void testGettChanges() {
+        CampaignRequestDto campaignRequestDto = new CampaignRequestDto(1L);
+        Campaign campaign = new Campaign(1L, "heheheh", "ahehahehahe", false, false, Category.OZEL_SAGLIK_SIGORTASI);
+
+        List<ChangesResponseDto> arr = new ArrayList<>();
+
+        Mockito.when(campaignRepository.findById(1L)).thenReturn(Optional.of(campaign));
+        Mockito.when(changersService.findByCampaign(campaign)).thenReturn(arr);
+        List<ChangesResponseDto> result = campaignService.getChanges(campaignRequestDto);
+
+        assertEquals(result, arr);
+
+    }
+
+    @Test
+    public void testactivateAndDeactivate() {
+        CampaignRequestDto campaignRequestDto = new CampaignRequestDto(1L);
+        Campaign campaign = new Campaign(1L, "heheheh", "ahehahehahe", false, false, Category.OZEL_SAGLIK_SIGORTASI);
+
+        CampaignResponseDto campaignResponseDto = new CampaignResponseDto("heheheh", "ahehahehahe");
+
+
+        Mockito.when(campaignRepository.findById(1L)).thenReturn(Optional.of(campaign));
+        Mockito.when(campaignRepository.save(campaign)).thenReturn(campaign);
+        Mockito.when(modelMapper.map(campaign, CampaignResponseDto.class)).thenReturn(campaignResponseDto);
+
+        CampaignResponseDto result = campaignService.activateAndDeactivate(campaignRequestDto);
+        assertEquals(result, campaignResponseDto);
+
+    }
 }
