2d29b3e41ee66277dae2992b6ec03adda233aec7
emrahyilm4z <emrahyilm4z@gmail.com>
2023-01-10 10:41:13 +0300

    Fixed problems

--- src/main/java/com/example/emrah/dto/request/CampaignRequestDto.java
+++ src/main/java/com/example/emrah/dto/request/CampaignRequestDto.java
@@ -2,9 +2,11 @@ package com.example.emrah.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CampaignRequestDto {
     private Long id;
 }
