2e38b114e4a3354516fbcd18b5d9b32eec22dbe0
emrahyilm4z <emrahyilm4z@gmail.com>
2023-01-10 10:38:29 +0300

    Fixed problems

--- src/main/java/com/example/emrah/dto/response/CampaignResponseDto.java
+++ src/main/java/com/example/emrah/dto/response/CampaignResponseDto.java
@@ -2,9 +2,11 @@ package com.example.emrah.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CampaignResponseDto {
     private String title;
     private String description;
